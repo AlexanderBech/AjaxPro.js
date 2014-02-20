1f9d466d642f1c4bd746eb017ebe77acf6a31a7e
Alexander Bech <github@alexanderbech.com>
2014-02-20 09:58:43 +0100

    Small improvements

--- jquery.ajaxpro.js
+++ jquery.ajaxpro.js
@@ -9,30 +9,41 @@
 	*	AJAX CALL
 	*/
 	window.ajaxPro = function(type, controller, $target, sendData, trigger, errorTrigger){
-		var type = type || 'GET';
-		var trigger = trigger || 'AjaxDone';
-		var errorTrigger = errorTrigger || false;
-		$.ajax({
-			type: type,
-			url: controller,
-			data: sendData,
-			dataType: 'json'
-		})
-		.done(function(data, textStatus, jqXHR){
-			if(data != null){
-				$target.trigger({ type:trigger, 'jsonData':data });
-				$target.off(trigger);
-			} else {
-				if(errorTrigger){
-					$target.trigger({ type:errorTrigger });
-					$target.off(errorTrigger);
-				} else {
-					alert('Error');
-				}
-			}
-		})
-		.fail(function(jqXHR, textStatus, errorThrown){
-			alert(errorThrown);
-		});
-	}
+        var type = type || 'GET';
+        var controller = controller || null;
+        var $target = $target || null;
+        var sendData = sendData || {};
+        var trigger = trigger || 'AjaxDone';
+        var errorTrigger = errorTrigger || false;
+        var ajaxSettings = {
+            type: type,
+            url: controller,
+            data: sendData,
+            dataType: 'json'
+        };
+        $.ajax(ajaxSettings)
+        .done(function(data, textStatus, jqXHR){
+            if(data != null){
+                $target.trigger({ type:trigger, 'jsonData':data });
+                $target.off(trigger);
+            } else {
+                if(errorTrigger){
+                    $target.trigger({ type:errorTrigger });
+                    $target.off(errorTrigger);
+                } else {
+                    alert('Error');
+                }
+            }
+        })
+        .fail(function(jqXHR, textStatus, errorThrown){
+            if (errorTrigger) {
+                $target.trigger({
+                    type: errorTrigger
+                });
+                $target.off(errorTrigger);
+            } else {
+                alert('Error: ' + errorThrown);
+            }
+        });
+    }
 })(jQuery);
